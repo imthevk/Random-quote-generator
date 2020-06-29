aaf06eda5e3b8289630b8be3c3a10d21b7becb1e
Vijay Kumar <vijaykumar@Vijays-MacBook-Pro.local>
2020-06-29 15:39:37 +0530

    js code added

---
+++ app.js
@@ -0,0 +1,45 @@
+const quoteContainer = document.getElementById("container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// Get quote from API
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    //if author is blank , add 'Unknown'
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    getQuote();
+  }
+}
+//
+// tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = quoteText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Event listeners
+newQuoteBtn.addEventListener("click", console.log("working"));
+twitterBtn.addEventListener("click", console.log("working"));
+
+//on Load
+getQuote();
