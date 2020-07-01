d746d81939bff961c4a743c25a6ea7056bdeb477
Vijay Kumar <vijaykumar@Vijays-MacBook-Pro.local>
2020-07-01 16:33:58 +0530

    app completed

--- app.js
+++ app.js
@@ -3,27 +3,46 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// show loading
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide loading
+function completeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get quote from API
 async function getQuote() {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  showLoadingSpinner();
+  //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+  // "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
-    const response = await fetch(proxyUrl + apiUrl);
+    const response = await fetch(apiUrl);
     const data = await response.json();
+    console.log(data.quote.quoteText);
     //if author is blank , add 'Unknown'
-    if (data.quoteAuthor === "") {
+    if (data.quote.quoteAuthor === "") {
       authorText.innerText = "Unknown";
     } else {
-      authorText.innerText = data.quoteAuthor;
+      authorText.innerText = data.quote.quoteAuthor;
     }
-    if (data.quoteText.length > 120) {
+    if (data.quote.quoteText.length > 120) {
       quoteText.classList.add("long-quote");
     } else {
       quoteText.classList.remove("long-quote");
     }
-    quoteText.innerText = data.quoteText;
+    quoteText.innerText = data.quote.quoteText;
+    //stop loader, show quote
+    completeLoadingSpinner();
   } catch (error) {
     getQuote();
   }
@@ -32,14 +51,14 @@ async function getQuote() {
 // tweet Quote
 function tweetQuote() {
   const quote = quoteText.innerText;
-  const author = quoteText.innerText;
+  const author = authorText.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(twitterUrl, "_blank");
 }
 
 //Event listeners
-newQuoteBtn.addEventListener("click", console.log("working"));
-twitterBtn.addEventListener("click", console.log("working"));
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //on Load
 getQuote();
